bc38e419ae5be16dad96bd330cfe23197ebf4cc2
akshay-somvanshi <akshaysomvanshi18@gmail.com>
2025-09-20 15:12:06 +0200

    Refactor and add firebase config

---
+++ src/config/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUe07FPbq5ko_W2Lt6IynMEhtnDcRonm4",
+  authDomain: "fir-project-4877c-dev.firebaseapp.com",
+  databaseURL: "https://fir-project-4877c-dev-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-project-4877c-dev",
+  storageBucket: "fir-project-4877c-dev.firebasestorage.app",
+  messagingSenderId: "1055448366204",
+  appId: "1:1055448366204:web:fd12287aee4806be47ddf9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
